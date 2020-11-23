8528c7f3ddc85f082d17ad477f7bb1ff03c5a234
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-22 18:48:21 -0800

    Add new routes for splash page links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   root to: "home#index"
-  get 'home/index'
-  
+
+  devise_for :users
   resources :outlines
+
+  get 'home/index'
+  get 'home/directions',  to: 'home#directions',  as: 'home_directions'
+  get 'home/about',       to: 'home#about',       as: 'home_about'
+  get 'home/contact',     to: 'home#contact',     as: 'home_contact'
+
+  get '/random',          to: 'random#new',       as: 'new_random'
+
+  get '/play',            to: 'play#new',         as: 'new_play'
   
 end
