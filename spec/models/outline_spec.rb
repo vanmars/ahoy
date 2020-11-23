74465c90344c3d4af6087ff0163582fd103addba
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-22 16:15:54 -0800

    Add validations and tests for users and outlines

--- spec/models/outline_spec.rb
+++ spec/models/outline_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 describe Outline do
   it { should belong_to(:user) }
+  
+  it { should validate_presence_of :claim_1 }
+  it { should validate_presence_of :evidence_1a }
+  it { should validate_presence_of :evidence_1b }
+  it { should validate_presence_of :close_call_1 }
+  it { should validate_presence_of :red_herring_1 }
+
+  it { should validate_presence_of :claim_2 }
+  it { should validate_presence_of :evidence_2a }
+  it { should validate_presence_of :evidence_2b }
+  it { should validate_presence_of :close_call_2 }
+  it { should validate_presence_of :red_herring_2 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 describe User do
-  it { should have_many(:outlines) }
+  it { should have_many :outlines }
+  it { should validate_presence_of :name }
 end
