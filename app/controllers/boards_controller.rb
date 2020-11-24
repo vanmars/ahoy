a79114c57f2ee2147bd657b03a6b2920a3f55845
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-23 17:12:09 -0800

    Format game board cards and sizing

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -26,25 +26,27 @@ class BoardsController < ApplicationController
     @outline1 = @board.outlines[0]
     @outline2 = @board.outlines[1]
 
-    @out1ev1a = Card.create({text: @outline1.evidence_1a, type: "blue_evidence", color:"blue"})
-    @out1ev1b = Card.create({text: @outline1.evidence_1b, type: "blue_evidence", color:"blue"})
-    @out1cc1 = Card.create({text: @outline1.close_call_1, type: "blue_close_call", color:"yellow"})
-    @out1rh1 = Card.create({text: @outline1.red_herring_1, type: "blue_red_herring", color:"red"})
-    @out1ev2a = Card.create({text: @outline1.evidence_2a, type: "blue_evidence", color:"blue"})
-    @out1ev2b = Card.create({text: @outline1.evidence_2b, type: "blue_evidence", color:"blue"})
-    @out1cc2 = Card.create({text: @outline1.close_call_2, type: "blue_close_call", color:"yellow"})
-    @out1rh2 = Card.create({text: @outline1.red_herring_2, type: "blue_red_herring", color:"red"})
+    @out1ev1a = Card.create({text: @outline1.evidence_1a, category: "blue_evidence", color:"blue"})
+    @out1ev1b = Card.create({text: @outline1.evidence_1b, category: "blue_evidence", color:"blue"})
+    @out1cc1 = Card.create({text: @outline1.close_call_1, category: "blue_close_call", color:"yellow"})
+    @out1rh1 = Card.create({text: @outline1.red_herring_1, category: "blue_red_herring", color:"red"})
+    @out1ev2a = Card.create({text: @outline1.evidence_2a, category: "blue_evidence", color:"blue"})
+    @out1ev2b = Card.create({text: @outline1.evidence_2b, category: "blue_evidence", color:"blue"})
+    @out1cc2 = Card.create({text: @outline1.close_call_2, category: "blue_close_call", color:"yellow"})
+    @out1rh2 = Card.create({text: @outline1.red_herring_2, category: "blue_red_herring", color:"red"})
 
-    @out2ev1a = Card.create({text: @outline2.evidence_1a, type: "green_evidence", color:"green"})
-    @out2ev1b = Card.create({text: @outline2.evidence_1b, type: "green_evidence", color:"green"})
-    @out2cc1 = Card.create({text: @outline2.close_call_1, type: "green_close_call", color:"yellow"})
-    @out2rh1 = Card.create({text: @outline2.red_herring_1, type: "green_red_herring", color:"red"})
-    @out2ev2a = Card.create({text: @outline2.evidence_2a, type: "green_evidence", color:"green"})
-    @out2ev2b = Card.create({text: @outline2.evidence_2b, type: "green_evidence", color:"green"})
-    @out2cc2 = Card.create({text: @outline2.close_call_2, type: "green_close_call", color:"yellow"})
-    @out2rh2 = Card.create({text: @outline2.red_herring_2, type: "green_red_herring", color:"red"})
+    @out2ev1a = Card.create({text: @outline2.evidence_1a, category: "green_evidence", color:"green"})
+    @out2ev1b = Card.create({text: @outline2.evidence_1b, category: "green_evidence", color:"green"})
+    @out2cc1 = Card.create({text: @outline2.close_call_1, category: "green_close_call", color:"yellow"})
+    @out2rh1 = Card.create({text: @outline2.red_herring_1, category: "green_red_herring", color:"red"})
+    @out2ev2a = Card.create({text: @outline2.evidence_2a, category: "green_evidence", color:"green"})
+    @out2ev2b = Card.create({text: @outline2.evidence_2b, category: "green_evidence", color:"green"})
+    @out2cc2 = Card.create({text: @outline2.close_call_2, category: "green_close_call", color:"yellow"})
+    @out2rh2 = Card.create({text: @outline2.red_herring_2, category: "green_red_herring", color:"red"})
 
-    @data = [ @out1ev1a, @out1ev1b, @out1cc1, @out1rh1, @out1ev2a, @out1ev2b, @out1cc2, @out1rh2, @out2ev1a, @out2ev1b, @out2cc1,  @out2rh1, @out2ev2a, @out2ev2b, @out2cc2, @out2rh2 ]
+    data = [ @out1ev1a, @out1ev1b, @out1cc1, @out1rh1, @out1ev2a, @out1ev2b, @out1cc2, @out1rh2, @out2ev1a, @out2ev1b, @out2cc1,  @out2rh1, @out2ev2a, @out2ev2b, @out2cc2, @out2rh2 ]
+
+    @data = data.shuffle
 
     # Determine First Turn
     random = rand(0..3)
