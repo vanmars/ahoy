960523689ee3dc0ba908e45c48d65885d8c2fbea
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-24 14:51:02 -0800

    Add functionality for game board cards to be clicked and turn colors

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -7,6 +7,14 @@ $(document).ready(function() {
 
   // Define variable to track mouse click.
   let clickTracker = [];
+
+  // Define a variable called 'turn'.
+  let turn;
+  if ($('#turn-text').html() === "Blue's Turn"){
+    turn = 'B'
+  } else {
+    turn = 'G'
+  }
   
   //When the blue count list length equals four, cue blue win scenario page.
   const blueWin = () => {
@@ -25,8 +33,6 @@ $(document).ready(function() {
   };
   
   // 2. THE GAME DETECTS WHOSE TURN IT IS AND PRINTS IT TO THE SCREEN.
- 
-  
     // Create function that defines how to change text, turn, clickTracker list on each turn scenario.
   const turnChange = (ev) => {
     if (turn == 'B' && clickTracker.length < 2){   // If turn is blue, and it is the first turn.
@@ -68,59 +74,40 @@ $(document).ready(function() {
       // Create Event Handlers
   const cardClicked = (ev) => {
     console.log(ev)
-    if (ev.target.classList.contains('blue')) {            // If clicked div card has a blue class tag, it will turn blue.
+    console.log(ev.target.classList)
+    console.log(ev.target.parentNode.classList)
+    console.log(ev.target.style)
+    console.log(ev.target.parentNode.style)
+
+    if (ev.target.classList.contains('blue')) {           
       ev.target.style.backgroundColor = '#4197E5';
-      flippedBlue.push(ev);                         // Append event to flippedBluelist.
-      blueWin();                                    // Initialize blueWin function to see if there is a blue win scneario.
-      clickTracker.push(ev);                          // Append event to clickTracker list to keep track of turn tacken.
-      turnChange(ev);                                 // Check to see if the turn needs to change to green.
-    } else if (ev.target.parentNode.classList.contains('blue')){     // In case users click on the text element rather than the div card, the div card will still change color, not just the text.
-      ev.parentNode.style.backgroundColor = '#4197E5';
+      flippedBlue.push(ev);                        
+      blueWin();                                                         
+    } else if (ev.target.parentNode.classList.contains('blue')){  
+      ev.target.parentNode.style.backgroundColor = '#4197E5';
       flippedBlue.push(ev);
       blueWin();
-      clickTracker.push(ev);
-      turnChange(ev);
-  
-    } else if (ev.target.className == 'green') {     // Same deal as abve, but for div cards with a purple class tag.
+
+
+    } else if (ev.target.classList.contains('green')) {     
       ev.target.style.backgroundColor = '#5F7511';
       flippedGreen.push(ev);
       greenWin();
-      clickTracker.push(ev);
-      turnChange(ev);
-    } else if (ev.path[1].className == 'green'){
-      ev.path[1].style.backgroundColor = '#5F7511';
+    } else if (ev.target.parentNode.classList.contains('green')){
+      ev.target.parentNode.style.backgroundColor  = '#5F7511';
       flippedGreen.push(ev);
       greenWin();
-      clickTracker.push(ev);
-      turnChange(ev);
-  
-    } else if (ev.target.className == 'red') {        // Same deal as above, but for div cards with a red class tag.
+    } else if (ev.target.classList.contains('red')) {        
       ev.target.style.backgroundColor = '#A54E4D';
-      clickTracker.push(ev);
-      turnChange(ev);
-    } else if (ev.path[1].className == 'red'){
-      ev.path[1].style.backgroundColor = '#A54E4D';
-      clickTracker.push(ev);
-      turnChange(ev);
-  
-    } else if (ev.target.className == 'blue_close_call') {     // Same deal as above, but for div cards with a ccyellow class tag.
-      ev.target.style.backgroundColor = '#F1AB5E';
-      clickTracker.push(ev);
-      turnChange(ev);
-    } else if (ev.path[1].className == 'blue_close_call'){
-      ev.path[1].style.backgroundColor = '#F1AB5E';
-      clickTracker.push(ev);
-      turnChange(ev);
-  
-    } else if (ev.path[1].className == 'green_close_call'){    // Same deal as above, but for div cards with a ccpurple class tag.
-      ev.path[1].style.backgroundColor = '#F1AB5E';
-      clickTracker.push(ev);
-      turnChange(ev);
-    } else {
-      ev.target.style.backgroundColor = '#F1AB5E';
-      clickTracker.push(ev);
-      turnChange(ev);
+    } else if (ev.target.parentNode.classList.contains('red')){
+      ev.target.parentNode.style.backgroundColor  = '#A54E4D';
+    } else if (ev.target.classList.contains('yellow')) {    
+      ev.target.style.backgroundColor  = '#F1AB5E';
+    } else if (ev.target.parentNode.classList.contains('yellow')){
+      ev.target.parentNode.style.backgroundColor  = '#F1AB5E';
     }
+    clickTracker.push(ev);
+    turnChange(ev);
   };
 
   // 4. User CAN TOGGLE THE VIEW
@@ -145,8 +132,7 @@ $(document).ready(function() {
     }
   }
   
-      // Create Event Listeners
-
+    // Event Listeners
     console.log("Event listeners attached!")
     $('#card0').click(cardClicked);
     $('#card1').click(cardClicked);
@@ -166,37 +152,29 @@ $(document).ready(function() {
     $('#card15').click(cardClicked);
     $('#game-view').click(viewToggler);
 
-
-    // Define a variable called 'turn'.
-    let turn;
-    if ($('#turn-text').html() === "Blue's Turn"){
-      turn = 'B'
-    } else {
-      turn = 'G'
-    }
-  
     
+  
   });
 
 
-  const changeCardColorByView = () => {
-    const view = document.querySelector('#game-view');
-    const cards = document.querySelectorAll('.board-card');
-    if (view.innerHTML === "Go To Islander's View") {
-      cards.forEach(function(card){
-        card.style.backgroundColor = '#0E4A71';
-      });
-    } else if (view.innerHTML === "Go To Captain's View") {
-      cards.forEach(function(card){
-        if (card.classList.contains('blue')){
-          card.style.backgroundColor = '#4197E5';
-        } else if (card.classList.contains('green')){
-          card.style.backgroundColor = '#5F7511';
-        } else if (card.classList.contains('yellow')){
-          card.style.backgroundColor = '#F1AB5E';
-        } else if (card.classList.contains('red')){
-          card.style.backgroundColor = '#A54E4D';
-        };
-      });
-    }
-  }
+  // const changeCardColorByView = () => {
+  //   const view = document.querySelector('#game-view');
+  //   const cards = document.querySelectorAll('.board-card');
+  //   if (view.innerHTML === "Go To Islander's View") {
+  //     cards.forEach(function(card){
+  //       card.style.backgroundColor = '#0E4A71';
+  //     });
+  //   } else if (view.innerHTML === "Go To Captain's View") {
+  //     cards.forEach(function(card){
+  //       if (card.classList.contains('blue')){
+  //         card.style.backgroundColor = '#4197E5';
+  //       } else if (card.classList.contains('green')){
+  //         card.style.backgroundColor = '#5F7511';
+  //       } else if (card.classList.contains('yellow')){
+  //         card.style.backgroundColor = '#F1AB5E';
+  //       } else if (card.classList.contains('red')){
+  //         card.style.backgroundColor = '#A54E4D';
+  //       };
+  //     });
+  //   }
+  // }
