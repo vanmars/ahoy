2b48e8a73f4ee3c2296743d1c91524ea04ba6412
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-18 10:52:36 -0800

    Fix bug in random board nomethoderror exception handler without knowing how?

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -9,10 +9,10 @@ module ExceptionHandler
       redirect_to(action: 'new')
     end
 
-    # rescue_from NoMethodError do |exception|
-    #   flash[:alert] = "Sorry! No record matches your search. Please try another title."
-    #   redirect_to(action: 'new')
-    # end
+    rescue_from NoMethodError do |exception|
+      flash[:alert] = "Sorry! No record matches your search. Please try another title."
+      redirect_to(action: 'new')
+    end
 
   end
 end
