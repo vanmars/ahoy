aac0b2a2df5715886a81df6201dcecd666d22212
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-22 12:30:39 -0800

    Comment out loading spinner js

--- app/assets/javascripts/random_game.js
+++ app/assets/javascripts/random_game.js
@@ -1,14 +1,14 @@
-const addSpinner = () => {
-  console.log("Spinner initiated!")
-  $('.loading').append('<div class="loading-spinner"></div>');
-}
+// const addSpinner = () => {
+//   console.log("Spinner initiated!")
+//   $('.loading').append('<div class="loading-spinner"></div>');
+// }
 
-const removeSpinner = () => {
-  console.log("Spinner hidden")
-  $('.loading-spinner').remove();
-}
+// const removeSpinner = () => {
+//   console.log("Spinner hidden")
+//   $('.loading-spinner').remove();
+// }
 
-$(document).ready(() => {
-  $('.load-spinner-btn').on('click', addSpinner)
-  $('.play-container').on('load', removeSpinner)
-});
+// $(document).ready(() => {
+//   $('.load-spinner-btn').on('click', addSpinner)
+//   $('.play-container').on('load', removeSpinner)
+// });
