70a9a30a9a2f6848ee4fd7585fa7812710fc9401
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-21 17:04:24 -0800

    Write and PASS test for random game board generation

---
+++ spec/features/random_game_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the generate a random game board" do
+  it 'generates a random game board' do
+    visit root_path
+    click_link 'Random'
+    fill_in 'Enter a Book Title', with: 'Hobbit'
+    click_on 'Play!'
+    expect(page).to have_content "Islander's View"
+  end
+
+  it 'gives an error when no book found in api' do
+    visit root_path
+    click_link 'Random'
+    fill_in 'Enter a Book Title', with: 'Hamlet'
+    click_on 'Play!'
+    expect(page).to have_content "Sorry! No record matches your search. Please try another title."
+  end
+end
