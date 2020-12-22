38c8869398bbfdd9414f4c66243dd5326cddffe8
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-21 18:12:40 -0800

    Write and PASS test for second link to about page

--- spec/features/about_pages_spec.rb
+++ spec/features/about_pages_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 describe "visiting the about page" do
-  it 'registers a new user' do
+  it 'sends users to the about page' do
     visit root_path
     click_link 'About'
+    expect(page).to have_content "Ahoy! is a collaborative, outline-intervention game that exposes writers to readers' reactions before writing a first draft."
+  end
+
+  it 'sends users to the about page' do
+    visit root_path
+    click_link 'Learn more about the theory behind the game design!'
     expect(page).to have_content "Ahoy! is a collaborative, outline-intervention game that exposes writers to readers' reactions before writing a first draft."
   end
 end
